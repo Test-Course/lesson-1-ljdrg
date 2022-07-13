5ee80bdd2871d2df13f45a80b9411087171e4332
ljdrg <108076548+ljdrg@users.noreply.github.com>
2022-07-13 14:27:42 +0200

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,1 +1,9 @@
-
+def internship_days(income,travel_costs, rent, other_expenses):
+    remaining_income = income - rent - other_expenses 
+    travel_days= remaining_income/(2*travel_costs)
+    if travel_days < 0:
+        return 0
+    if travel_days > 30:
+        return 30
+    else:
+        return int(travel_days)
